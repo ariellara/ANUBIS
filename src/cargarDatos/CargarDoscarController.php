a3aecf28c29da7440123556def7d04ae1c3b37e7
ariel.lara <ariel.lara@dolphining.com>
2025-09-07 20:16:18 -0500

    papa

--- src/cargarDatos/CargarDoscarController.php
+++ src/cargarDatos/CargarDoscarController.php
@@ -1,59 +1,81 @@
-
-
 <?php
-require_once '../comunes/Respuesta.php';
-require_once '../../conexion/conexion.php';
-require_once 'CargarDoscarManager.php';
+/*
+*Autor: Ariel Lara
+*Fecha: 2025-09-07
+*Descripción: Controlador para manejar las solicitudes de sincronización de datos.
+*/
+require_once __DIR__ . '/../comunes/Respuesta.php';
+require_once __DIR__ . '/../../conexion/conexion.php';
+require_once __DIR__ . '/CargarDoscarManager.php';
 
-header('Content-Type: application/json');
-
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    echo json_encode(["success" => false, "mensaje" => "Método no permitido"]);
-    exit;
-}
-
-$raw_data = file_get_contents("php://input");
-$data = json_decode($raw_data, true);
-usleep(500000);
-if (!is_array($data)) {
-    http_response_code(400);
-    echo json_encode(["success" => false, "mensaje" => "JSON inválido"]);
-    exit;
-}
-$respuesta = new Respuesta();
-try {
-    $cargarDoscarManager = new CargarDoscarManager($conexion);
-
-    switch ($data["tipo"] ?? "") {
-        case "CARGARDOSCAR":
-            if (!isset($data)) {
-                throw new Exception("Falta los datos");
-            }
-            $respuesta = $cargarDoscarManager->sincronizarDatos($data);
-            $respuesta->setUrl("");
-            break;
-         case "CARGARDATOSNUBE":
-            if (!isset($data)) {
-                throw new Exception("Falta los datos");
-            }
-            $respuesta = $cargarDoscarManager->cargarDatosNube($data);
-            $respuesta->setUrl("");
-            break;   
-
-        default:
-            $respuesta->setSuccess(false);
-            $respuesta->setMensaje("Operación desconocida");
-            $respuesta->setDatos([]);
-            $respuesta->setUrl("");
-            break;
+class ControladorSincronizacion
+{
+    private $conexion;
+    private $cargarDoscarManager;
+    private $respuesta;
+    public function __construct($conexion)
+    {
+        $this->conexion = $conexion;
+        $this->cargarDoscarManager = new CargarDoscarManager($conexion);
+        $this->respuesta = new Respuesta();
     }
 
-} catch (Exception $e) {
-    $respuesta->setSuccess(false);
-    $respuesta->setMensaje("Error en el servidor: " . $e->getMessage());
-    $respuesta->setDatos([]);
-    $respuesta->setUrl("");
-}
+    public function procesarRequest()
+    {
+        header('Content-Type: application/json');
 
-echo $respuesta->toJson();
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405);
+            echo json_encode(["success" => false, "mensaje" => "Método no permitido"]);
+            exit;
+        }
+        $raw_data = file_get_contents("php://input");
+        $data = json_decode($raw_data, true);
+        usleep(500000);
+
+        if (!is_array($data)) {
+            http_response_code(400);
+            echo json_encode(["success" => false, "mensaje" => "JSON inválido"]);
+            exit;
+        }
+        try {
+            $this->procesarOperacion($data);
+        } catch (Exception $e) {
+            $this->respuesta->setSuccess(false);
+            $this->respuesta->setMensaje("Error en el servidor: " . $e->getMessage());
+            $this->respuesta->setDatos([]);
+            $this->respuesta->setUrl("");
+        }
+        echo $this->respuesta->toJson();
+    }
+
+    private function procesarOperacion($data)
+    {
+        switch ($data["tipo"] ?? "") {
+            case "CARGARDOSCAR":
+                if (!isset($data)) {
+                    throw new Exception("Faltan los datos");
+                }
+                $this->respuesta = $this->cargarDoscarManager->sincronizarDatos($data);
+                $this->respuesta->setUrl("");
+                break;
+
+            case "CARGARDATOSNUBE":
+                if (!isset($data)) {
+                    throw new Exception("Faltan los datos");
+                }
+                $this->respuesta = $this->cargarDoscarManager->cargarDatosNube($data);
+                $this->respuesta->setUrl("");
+                break;
+
+            default:
+                $this->respuesta->setSuccess(false);
+                $this->respuesta->setMensaje("Operación desconocida");
+                $this->respuesta->setDatos([]);
+                $this->respuesta->setUrl("");
+                break;
+        }
+    }
+}
+$controlador = new ControladorSincronizacion($conexion);
+$controlador->procesarRequest();
